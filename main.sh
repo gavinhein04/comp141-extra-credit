3e963d9ba00a85e863b06a7c423931b6ef1fbd92
gavinhein04 <157073424+gavinhein04@users.noreply.github.com>
2024-04-23 02:27:44 +0000

    Add main.sh to the project.

--- main.sh
+++ main.sh
@@ -1,1 +1,16 @@
-main.sh
+mkdir output
+mv my_file.txt output
+cd output
+cat my_file.txt > read.txt
+pwd > pwd.txt && ls > ls.txt
+cp my_file.txt copy.txt
+alias date="date"
+date > date.txt
+wc -w < my_file.txt > textcount.txt
+ps > ps.txt && head -5 ps.txt > process.txt
+ifconfig > ifconfig.txt && head -5 ifconfig.txt > netstat.txt
+mount > mount1.txt && head -5 mount1.txt > mount.txt
+touch permissions.txt && chmod 777 permissions.txt
+TESTENV1="test"
+grep -Ei '([a-z]{3,})' my_file.txt
+cd ..
